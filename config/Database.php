d2831375f9f19296406388725bbc6f9c12340b70
zakaria <zikokh012@gmail.com>
2023-12-20 10:01:53 +0100

    conenction avec databse

---
+++ config/Database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'intranet';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function getConnect() {
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}",$this->username, $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo 'conencted';
+            
+        } catch (PDOException $exception) {
+            echo 'error de conection: ' . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+$Database = new Database();
+$Database->getConnect();
+
+
+?>
