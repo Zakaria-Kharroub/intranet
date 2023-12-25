a9e3dacee8988e36edf1330468efd418f38b41ab
zakaria <zikokh012@gmail.com>
2023-12-25 01:33:10 +0100

    update

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <?php
-
 include 'controllers/UserController.php';
 
 $UserController = new UserController();
 
 if (isset($_GET['action'])) {
-    $submit = $_GET['action'];
+    $submit= $_GET['action'];
     switch ($submit) {
         case 'listFormateurs':
             $UserController->listFormateurs();
             break;
+
+
         case 'ajouterFormateur': 
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                
+
                 $nom = $_POST['nom'];
+              
                 $prenom = $_POST['prenom'];
-                $email = $_POST['email'];
+                $email =$_POST['email'];
+                $password = $_POST['password'];
+                $id_class= $_POST['id_class'];
+
+                $UserController->ajouterFormateur($nom, $prenom,$email, $password,$id_class);
+            }
+            break;
+        case 'updateFormateur':
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $id_utilisateur =$_POST['id_utilisateur'];
+                $nom =$_POST['nom'];
+                $prenom =$_POST['prenom'];
+                $email= $_POST['email'];
                 $password = $_POST['password'];
                 $id_class = $_POST['id_class'];
 
-                
-                $UserController->ajouterFormateur($nom, $prenom, $email, $password, $id_class);
+                $UserController->updateFormateur($id_utilisateur, $nom, $prenom, $email, $password,$id_class);
             }
             break;
         default:
@@ -30,6 +43,7 @@ if (isset($_GET['action'])) {
 } else {
     $UserController->listUsers();
 }
+
 
 
 
